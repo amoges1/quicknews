0692745da73c276ce104a3c385f43d4d24014c08
Anteneh M <amoges1@gmail.com>
2018-12-26 22:15:14 -0500

    added sqlite3 article archiving, article dates

--- quicknews.py
+++ quicknews.py
@@ -1,35 +1,114 @@
 from bs4 import BeautifulSoup
 import requests
 import jinja2
+import sqlite3
+import dateutil.parser
 import webbrowser
+
 
 class Article:
     """Article contains title and bullets."""
-    def __init__(self, title, bullets, url):
+    def __init__(self, title, bullets, url, date):
         self.title = title
         self.bullets = bullets
         self.url = url
+        self.date = date
 
+    def getValues(self):
+        return self.title, self.bullets, self.url, self.date
+
+#
+# Extract only Date and Time ISO to readable format 
+#
+def convertISO(timestamp):
+    isoDate = timestamp.split("+")[0]
+    dateTime = " ".join(isoDate.split("T"))
+    return dateTime
+
+#
+# Extract an article's title, bullet summary, published date and url   
+#
 def extract_article(url):
 
     # get website page
     res = requests.get(url)
-
+    
     # parse website html
     soup = BeautifulSoup(res.text, 'html.parser')
 
     title = soup.find('h1', {'class':'post-headline'})
-    bullets = soup.find('ul', {'class':'summary-list'})
+    strTitle = title.getText(strip=True).replace("'", "''")
+    summary = soup.find('ul', {'class':'summary-list'})
+   
+    timestamp = soup.find('div', {'class': 'byline-timestamp'})["data-timestamp"]
+    date = convertISO(timestamp)
+    
 
     # Not all articles have bullets
-    if title and bullets:
-        return title.getText(), bullets
-    return title.getText(), "Click above to learn more"
+    if title and summary:
 
+         # convert summary into lists, remove end whitespace
+        summary = summary.getText().split(".")[:-1]
 
-def scrape_tech_insider(url='http://www.businessinsider.com/sai'):
-    # print("Now loading today's articles...")
+        # remove whitespaces in each bullet
+        bullets = [ bullet.strip() for bullet in summary]
+
+        # escape apostrophes by replacement
+        strBullets = ".".join(bullets).replace("'", "''")
+        return strTitle, strBullets, date
+    return strTitle, "Click above to learn more", date
+
+#
+# Insert web scraped articles into SQLite3 DB, Table News for archive
+#
+def archive_insert(conn, article):
+    c = conn.cursor()
+    strTitle, strBullets, date, url = article.getValues()
+
+    # insert new article 
+    sql_query = """INSERT OR IGNORE INTO NEWS (TITLE, SUMMARY, DATE, URL) VALUES ('{}', '{}', '{}', '{}')""".format(strTitle, strBullets, date, url)
+    c.execute(sql_query)
+    conn.commit()
+
+#
+# Import archived articles in SQLite3 DB, Table NEWS 
+#
+def import_archive(conn):
+    news = []
+
+    c = conn.cursor()
+    c.execute("SELECT * FROM NEWS")
+
+    for row in c:
+        title, bullets, url, date = row[0], row[1], row[2], row[3]
+        article = Article(title, bullets, url, date)
+        news.append(article)
+
+    return news
+
+#
+# Remove articles when DB limit of 32 is reached
+# 
+def remove_archives(conn):
+    c = conn.cursor()
+    count = c.execute("SELECT COUNT(TITLE) FROM NEWS").fetchone()[0]
     
+    # remove old articles
+    if(count < 32):
+        c.execute("""DELETE FROM NEWS WHERE ROWID > 1 """)
+        conn.commit()
+
+#
+# Driver function to retrieve articles from BI Tech page
+#     
+def scrape_tech_insider(url='http://www.businessinsider.com/sai'):
+    # connect to Archive database
+    conn = sqlite3.connect('static/archive.db')
+    
+    # remove old articles
+    remove_archives(conn)
+    
+    # setup webscrape
     response = requests.get(url)
     text = response.text
     soup = BeautifulSoup(text, 'html.parser')
@@ -38,16 +117,17 @@ def scrape_tech_insider(url='http://www.businessinsider.com/sai'):
     article_links = soup.find_all('a', { 'class':'title'})
     article_urls = [ a['href'] for a in article_links]
 
-    news = []
+    # insert articles into archive
     for url in article_urls:
-        title, bullets = extract_article(url)
-        # print(bullets)
+        title, bullets, date = extract_article(url)
         if title and bullets:
-            article = Article(title, bullets, url)
-            news.append(article)
+            article = Article(title, bullets, url, date)
+            archive_insert(conn, article)
+    
+    # import archived articles
+    news = import_archive(conn)
 
-    # for articles in news:
-    #     print(articles.title)
+    conn.close()
 
     # Jinja2 setup
     env = jinja2.Environment()
@@ -65,4 +145,5 @@ def scrape_tech_insider(url='http://www.businessinsider.com/sai'):
     # open new file filled with articles
     webbrowser.open('article.html')
 
-scrape_tech_insider()
+if __name__ == "__main__":
+    scrape_tech_insider()
