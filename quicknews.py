b9da7a550b908cb57e75f03fde9f9900fde8a137
Anteneh M <amoges1@gmail.com>
2019-01-05 12:22:57 -0500

    comment/update functions relating to sql db,  article symbol codes, iso/datetime

--- quicknews.py
+++ quicknews.py
@@ -8,25 +8,17 @@ import webbrowser
 
 class Article:
     """Article contains title and bullets."""
-    def __init__(self, title, bullets, url, date):
+    def __init__(self, title, bullets, date, url):
         self.title = title
         self.bullets = bullets
-        self.url = url
         self.date = date
+        self.url = url
 
     def getValues(self):
-        return self.title, self.bullets, self.url, self.date
+        return self.title, self.bullets, self.date, self.url
 
 #
-# Extract only Date and Time ISO to readable format 
-#
-def convertISO(timestamp):
-    isoDate = timestamp.split("+")[0]
-    dateTime = " ".join(isoDate.split("T"))
-    return dateTime
-
-#
-# Extract an article's title, bullet summary, published date and url   
+# Extract an article's title, bullet summary, url, and published date   
 #
 def extract_article(url):
 
@@ -37,26 +29,49 @@ def extract_article(url):
     soup = BeautifulSoup(res.text, 'html.parser')
 
     title = soup.find('h1', {'class':'post-headline'})
-    strTitle = title.getText(strip=True).replace("'", "''")
-    summary = soup.find('ul', {'class':'summary-list'})
-   
-    timestamp = soup.find('div', {'class': 'byline-timestamp'})["data-timestamp"]
-    date = convertISO(timestamp)
-    
+    # strip beg/end whitespaces, escape apostrophes, hyphens w/ html code (for sql/viewing)
+    strTitle = title.getText(strip=True).replace("'", "&#39;").replace("’", "&#39;").replace("—", "&#8208;")
 
+    # find <li> bullet points
+    summary = soup.find('ul', {'class':'summary-list'})
+    
+    # find timestamp in ISO 8601 format e.g. 2019-01-04T23:58:22+0000
+    timestamp = soup.find('div', {'class': 'byline-timestamp'})["data-timestamp"]
+
+    # remove timezone
+    date = timestamp.split("+")[0] 
+    
     # Not all articles have bullets
     if title and summary:
 
-         # convert summary into lists, remove end whitespace
-        summary = summary.getText().split(".")[:-1]
-
-        # remove whitespaces in each bullet
-        bullets = [ bullet.strip() for bullet in summary]
-
-        # escape apostrophes by replacement
-        strBullets = ".".join(bullets).replace("'", "''")
+        # convert summary into lists, switch quotes." to ". and remove last-index whitespace
+        listSummary = summary.getText().replace(".\"", "\".").split(".")[:-1]
+        # remove/cleanse whitespaces in each bullet
+        bullets = [ bullet.strip() for bullet in listSummary]
+        # escape apostrophes, hyphens w/ html code (for sql/viewing)
+        strBullets = ".".join(bullets).replace("'", "&#39;").replace("’", "&#39;").replace("—", "&#8208;")
+        
         return strTitle, strBullets, date
+
+    # No summary, add simple text for return
     return strTitle, "Click above to learn more", date
+
+#
+# Import archived articles from sql database, table NEWS, DESC Order
+#
+def import_archive(conn):
+    c = conn.cursor()
+    c.execute("SELECT * FROM NEWS ORDER BY DATE DESC")
+
+    news = []
+    # Export news from sql archive db in chronological order
+    for row in c:
+        title, bullets, dateISO, url = row[0], row[1], row[2], row[3]
+        date = dateutil.parser.parse(dateISO) # convert ISO into datetime object
+        article = Article(title, bullets, date, url)
+        news.append(article)
+
+    return news
 
 #
 # Insert web scraped articles into SQLite3 DB, Table News for archive
@@ -64,65 +79,52 @@ def extract_article(url):
 def archive_insert(conn, article):
     c = conn.cursor()
     strTitle, strBullets, date, url = article.getValues()
-
+    
     # insert new article 
     sql_query = """INSERT OR IGNORE INTO NEWS (TITLE, SUMMARY, DATE, URL) VALUES ('{}', '{}', '{}', '{}')""".format(strTitle, strBullets, date, url)
     c.execute(sql_query)
     conn.commit()
 
 #
-# Import archived articles in SQLite3 DB, Table NEWS 
-#
-def import_archive(conn):
-    news = []
-
-    c = conn.cursor()
-    c.execute("SELECT * FROM NEWS ORDER BY ROWID DESC")
-
-    for row in c:
-        title, bullets, url, date = row[0], row[1], row[2], row[3]
-        article = Article(title, bullets, url, date)
-        news.append(article)
-
-    return news
-
-#
-# Remove articles when DB limit of 32 is reached
+# Remove old articles from sql database
 # 
 def remove_archives(conn):
     c = conn.cursor()
-    count = c.execute("SELECT COUNT(TITLE) FROM NEWS").fetchone()[0]
-    
-    # remove old articles
-    if(count < 32):
-        c.execute("""DELETE FROM NEWS WHERE ROWID > 1 """)
-        conn.commit()
+    c.execute("""DELETE FROM NEWS""")
+    conn.commit()
+       
+def check_table(conn):
+    c = conn.cursor()
+    sql_query = """CREATE TABLE IF NOT EXISTS NEWS (TITLE TEXT, SUMMARY TEXT, DATE TEXT, URL TEXT, UNIQUE(TITLE)) """
+    c.execute(sql_query)
 
 #
 # Driver function to retrieve articles from BI Tech page
 #     
 def scrape_tech_insider(url='http://www.businessinsider.com/sai'):
-    # connect to Archive database
+    # create/connect to Archive database
     conn = sqlite3.connect('static/archive.db')
     
+    # create table if none exists
+    check_table(conn)
+
     # remove old articles
     remove_archives(conn)
     
     # setup webscrape
-    response = requests.get(url)
-    text = response.text
-    soup = BeautifulSoup(text, 'html.parser')
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
 
-    # Parse Tech BI page for all articles' urls
-    article_links = soup.find_all('a', { 'class':'title'})
-    article_urls = [ a['href'] for a in article_links]
+    # Find <a> headlines on BI Tech Page
+    article_headlines = soup.find_all('a', { 'class':'title'})
+    # Extract urls of respective <a> headlines
+    article_urls = [ a['href'] for a in article_headlines]
 
-    # insert articles into archive
+    # insert articles into sql archive db
     for url in article_urls:
         title, bullets, date = extract_article(url)
-        if title and bullets:
-            article = Article(title, bullets, url, date)
-            archive_insert(conn, article)
+        article = Article(title, bullets, date, url)
+        archive_insert(conn, article)
     
     # import archived articles
     news = import_archive(conn)
